9c397860e10fda6a195948a19a1e176ee9c594b2
cwd曹文栋 <wdcao@ctrip.com>
2018-03-12 00:03:13 +0800

    add code doc

--- study-spring/src/main/java/com/saint/design/mode/adapter/Main.java
+++ study-spring/src/main/java/com/saint/design/mode/adapter/Main.java
@@ -1,5 +1,14 @@
 package com.saint.design.mode.adapter;
 
+/**
+ * 这是学习适配器设计模式的主函数
+ *
+ * 适配器模式：
+ *      适配器类Adapter
+ *      被适配的类Adaptee
+ *      目标接口Target
+ *      已经实现Target的参照接口ConcreteTarget
+ */
 public class Main {
     public static void main(String[] args){
         Target concreteTarget = new ConcreteTarget();
