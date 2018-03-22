ae5e24703209a789bda897810d75169031abf5a2
cwd曹文栋 <wdcao@ctrip.com>
2018-03-23 00:13:54 +0800

    add iterator model

---
+++ study-spring/src/main/java/com/saint/design/mode/iterator/Main.java
@@ -0,0 +1,22 @@
+package com.saint.design.mode.iterator;
+
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args){
+        ArrayIterator arrayIterator = new ArrayIterator();
+        ArrayListIterator arrayListIterator = new ArrayListIterator();
+
+        printIterator(arrayIterator);
+        System.out.println("other iterator");
+        printIterator(arrayListIterator);
+    }
+
+    public static void printIterator(Iterator<String> iterator){
+        while (iterator.hasNext()){
+            String iteratorValue = iterator.next();
+            System.out.println("iteratorValue: "+iteratorValue);
+        }
+    }
+}
