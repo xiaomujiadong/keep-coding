0722fe241990b9f254dea9afa2f241ecac791582
cwd曹文栋 <wdcao@ctrip.com>
2018-04-19 00:47:01 +0800

    add use threadlocal demo

--- study-demo/src/main/java/com/saint/demo/thread/ThreadLocalMain.java
+++ study-demo/src/main/java/com/saint/demo/thread/ThreadLocalMain.java
@@ -8,19 +8,36 @@ public class ThreadLocalMain {
     public static void main(String[] args){
         System.out.println("tset");
         for (int i = 0; i < 2; i++) {
-            new Thread(new Runnable() {
-                public void run() {
-                    int data = new Random().nextInt();
-                    System.out.println(Thread.currentThread().getName()
-                            + " has put data :" + data);
-                    x.set(data);
-
-                    new SonThreadLocal(x).get();
-                    new B(x).get();
-                }
-            }).start();
+            new Thread(new LocalThread(i, x)).start();
         }
 
+    }
+}
+class LocalThread implements Runnable{
+    private int id;
+    private ThreadLocal<Integer> threadLocal;
+    public LocalThread(int id, ThreadLocal threadLocal){
+        this.id = id;
+        this.threadLocal = threadLocal;
+    }
+    @Override
+    public void run() {
+        int data = new Random().nextInt();
+        System.out.println(Thread.currentThread().getName()
+                + " has put data :" + data);
+        threadLocal.set(data);
+
+        new SonThreadLocal(threadLocal).get();
+
+        if(id==0){
+            try {
+                Thread.sleep(2*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        new B(threadLocal).get();
     }
 }
 class SonThreadLocal{
