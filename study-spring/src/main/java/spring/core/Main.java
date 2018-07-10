9f7edff966792aa9cd63c4a4ec694a6bc1777550
cwd曹文栋 <wdcao@ctrip.com>
2018-07-10 22:38:40 +0800

    add hadoop module

--- study-spring/src/main/java/spring/core/Main.java
+++ study-spring/src/main/java/spring/core/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args){
-        AbstractXmlApplicationContext context = new ClassPathXmlApplicationContext("spring\\applicationContext.xml");
+        AbstractXmlApplicationContext context = new ClassPathXmlApplicationContext("spring\\applicationContextSpring.xml");
         User user = (User)context.getBean("user");
 
         user.info();
--- study-spring/src/main/java/spring/core/User.java
+++ study-spring/src/main/java/spring/core/User.java
@@ -1,7 +1,14 @@
 package spring.core;
 
+import org.springframework.transaction.annotation.Transactional;
+
 public class User {
 
+    public User(){
+        System.out.println("---------------------------------user is creating");
+    }
+
+    @Transactional
     public void info(){
         System.out.println("info");
     }
